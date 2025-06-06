e524505e66f8e9df79773d7127c06f430f20aef3
Roel Crabbé <crabberoel@gmail.com>
2025-06-06 21:34:16 +0200

    Prettier: Auto Formatting

--- src/shared/types/role.ts
+++ src/shared/types/role.ts
@@ -1,11 +1,11 @@
 export const Role = {
     Admin: 'Admin',
-    HumanResources: 'HumanResources',
+    HumanResources: 'Human Resources',
     Guest: 'Guest',
 } as const;
 
 export type Role = (typeof Role)[keyof typeof Role];
 
 export const isValidRole = (role: unknown): role is Role => {
-    return typeof role === 'string' && role in Role;
+    return typeof role === 'string' && Object.values(Role).includes(role as Role);
 };
